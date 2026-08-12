e6bcd4b5ff78cf911a86961fd9cdc320c3cb550c
h55d <large.nut1046@fastmail.com>
2026-08-12 20:03:01 +0200

    fix georg plasa

--- tests/modules/car-helper.test.js
+++ tests/modules/car-helper.test.js
@@ -14,7 +14,9 @@ describe('R3ECarUtils', () => {
     test('splits car names including special cases', () => {
         expect(window.R3ECarUtils.splitCarName('Audi R8 LMS')).toEqual({ brand: 'Audi', model: 'R8 LMS' });
         expect(window.R3ECarUtils.splitCarName('E36 V8 JUDD')).toEqual({ brand: 'Georg Plasa', model: 'BMW E36 V8' });
+        expect(window.R3ECarUtils.splitCarName('BMW E36 V8 JUDD')).toEqual({ brand: 'Georg Plasa', model: 'BMW E36 V8' });
         expect(window.R3ECarUtils.splitCarName('134 Judd V8')).toEqual({ brand: 'Georg Plasa', model: 'BMW 134 V8' });
+        expect(window.R3ECarUtils.splitCarName('BMW 134 Judd V8')).toEqual({ brand: 'Georg Plasa', model: 'BMW 134 V8' });
         expect(window.R3ECarUtils.splitCarName('Carlsson SLK 340 JUDD')).toEqual({ brand: 'Carlsson', model: 'Mercedes SLK 340' });
         expect(window.R3ECarUtils.splitCarName('BMW Alpina B6 GT3')).toEqual({ brand: 'BMW Alpina', model: 'B6 GT3' });
         expect(window.R3ECarUtils.splitCarName('DTM Mercedes AMG C-Coupé')).toEqual({ brand: 'Mercedes-AMG', model: 'C-Coupé' });
@@ -27,7 +29,9 @@ describe('R3ECarUtils', () => {
         expect(window.R3ECarUtils.resolveBrandLogoPath('McLaren 720S GT3')).toBe('images/brands/logo-mclaren.png');
         expect(window.R3ECarUtils.resolveBrandLogoPath('Lynk & Co 03 TCR')).toBe('images/brands/logo-lynk-co.png');
         expect(window.R3ECarUtils.resolveBrandLogoPath('134 Judd V8')).toBe('images/brands/logo-georg-plasa.png');
+        expect(window.R3ECarUtils.resolveBrandLogoPath('BMW 134 Judd V8')).toBe('images/brands/logo-georg-plasa.png');
         expect(window.R3ECarUtils.resolveBrandLogoPath('E36 V8 JUDD')).toBe('images/brands/logo-georg-plasa.png');
+        expect(window.R3ECarUtils.resolveBrandLogoPath('BMW E36 V8 JUDD')).toBe('images/brands/logo-georg-plasa.png');
         expect(window.R3ECarUtils.resolveBrandLogoPath('BMW Alpina B6 GT3')).toBe('images/brands/logo-alpina.png');
         expect(window.R3ECarUtils.resolveBrandLogoPath('DTM Mercedes AMG C-Coupé')).toBe('images/brands/logo-mercedes.png');
         expect(window.R3ECarUtils.resolveBrandLogoPath('')).toBe('images/brands/logo-raceroom.png');
